abaa2f4d0f07bb7dfc73a3daf42be78536c7b712
denysartiukhov <artiukhov.denys@gmail.com>
2024-06-10 23:34:33 -0500

    replace some elements with material widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,32 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:habit_tracker/widgets/habit_tracker.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 void main() {
   databaseFactory = databaseFactoryFfi;
   runApp(
-    const CupertinoApp(
-      theme: CupertinoThemeData(
+    MaterialApp(
+      theme: ThemeData(
         brightness: Brightness.dark,
-        textTheme: CupertinoTextThemeData(
-          textStyle: TextStyle(
-            color: CupertinoColors.white,
-          ),
-          navTitleTextStyle: TextStyle(
-            color: CupertinoColors.white,
-          ),
-          navLargeTitleTextStyle: TextStyle(
-            color: CupertinoColors.white,
-            fontSize: 20,
-          ),
-        ),
-        primaryColor: CupertinoColors.systemBlue,
-        primaryContrastingColor: Color.fromARGB(255, 44, 44, 44),
-        scaffoldBackgroundColor: CupertinoColors.black,
       ),
+      // theme: ThemeData(
+      //   brightness: Brightness.dark,
+      //   textTheme: CupertinoTextThemeData(
+      //     textStyle: TextStyle(
+      //       color: CupertinoColors.white,
+      //     ),
+      //     navTitleTextStyle: TextStyle(
+      //       color: CupertinoColors.white,
+      //     ),
+      //     navLargeTitleTextStyle: TextStyle(
+      //       color: CupertinoColors.white,
+      //       fontSize: 20,
+      //     ),
+      //   ),
+      //   primaryColor: CupertinoColors.systemBlue,
+      //   primaryContrastingColor: Color.fromARGB(255, 44, 44, 44),
+      //   scaffoldBackgroundColor: CupertinoColors.black,
+      // ),
       home: HabitTracker(),
     ),
   );
